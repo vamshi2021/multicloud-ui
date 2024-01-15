0ba31a88dc28355d9e9b6d56d479376529348c42
vamshi2021 <vamshich018@gmail.com>
2024-01-15 19:35:28 +0530

    Revert "Merge branch 'development' of https://github.com/vamshi2021/multicloud-ui into development"
    This reverts commit fb0827da0cad03633d2c9b8f0ac07e0faeaec26e, reversing
    changes made to 10093bb066dafb2ccac2a05a30e5193c183e953d.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import { React, Suspense } from 'react';
+import React,{ useState, Suspense } from 'react';
 import './App.css';
+import { GET_STUDENT_LIST_DETAILS } from './graphql/queries';
+import {INSERT_STUDENT_LIST, UPDATE_STUDENT_LIST, DELETE_STUDENT_LIST} from './graphql/mutation'
+import { useQuery, useMutation} from '@apollo/client';
 import LandingPage from './stores/pages/PageLanding';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
 import MobilePage from './stores/pages/MobilepageList';
 import WatchPage from './stores/pages/WatchpageList';
 import Furniturepage from './stores/pages/Furniturepage'; 
@@ -13,27 +17,29 @@ import WatchSingle from './stores/singles/WatchSingle';
 import UserCart from './stores/UserCart';
 
 function App() {
-    return (
-        <Router>
-        <div>
-          <hr/>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route path="/" element={<LandingPage />} /> 
-              <Route path="/Mobiles" element={<MobilePage />} />
-              <Route path="/Furniture" element={<Furniturepage />} />
-              <Route path="/Watch" element={<WatchPage />} />
-              <Route path="/Ac" element={<Acpage/>} />
-                <Route path='/Mobile/:id' element = {<MobileSingle />} />
-                <Route path='/Furniture/:id' element = {<FurnitureSingle />} />
-                <Route path='/Watch/:id' element = {<WatchSingle />} />
-                <Route path='/Ac/:id' element = {<AcSingle />} />
-              <Route path='/cart' element = {<UserCart />} />
-            </Routes>
-          </Suspense>
-        </div>
-      </Router>
-    );
+ 
+  return (
+      <Router>
+      <div>
+        <hr/>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<LandingPage />} /> 
+            <Route path="/Mobiles" element={<MobilePage />} />
+            <Route path="/Furniture" element={<Furniturepage />} />
+            <Route path="/Watch" element={<WatchPage />} />
+            <Route path="/Ac" element={<Acpage/>} />
+              <Route path='/Mobile/:id' element = {<MobileSingle />} />
+              <Route path='/Furniture/:id' element = {<FurnitureSingle />} />
+              <Route path='/Watch/:id' element = {<WatchSingle />} />
+              <Route path='/Ac/:id' element = {<AcSingle />} />
+            <Route path='/cart' element = {<UserCart />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
+    
+  );
 }
-
+ 
 export default App;
